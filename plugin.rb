79fe9771ba84b0b8516b0ebf73621999751df7e1
jose-hms <jose@hms.se>
2018-03-19 14:44:05 -0400

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: email-topic-generation
+# about: Create topic and invite users with a button click
+# version: 0.1
+# authors: Jordan Seanor
+# url: https://github.com/HMSAB/discourse-email-generation.github
