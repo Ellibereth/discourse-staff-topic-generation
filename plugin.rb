c4b27d49b3ac84439cc832d9411318b7ffdf7eab
jose-hms <jose@hms.se>
2018-03-21 14:40:57 -0400

    updated plugin.rb with needed items

--- plugin.rb
+++ plugin.rb
@@ -2,4 +2,16 @@
 # about: Create topic and invite users with a button click
 # version: 0.1
 # authors: Jordan Seanor
-# url: https://github.com/HMSAB/discourse-email-generation.github
+# url: https://github.com/HMSAB/discourse-email-generation.git
+
+enabled_site_setting :email_topic_generation_enabled
+
+register_asset("stylesheets/staffmail.scss", :desktop)
+
+after_initialize do
+  load File.expand_path("../controllers/staffmail_controller.rb", __FILE__)
+
+  Discourse::Application.routes.prepend do
+    post 'staffmail/send_notification' => 'staffmail#send_notification'
+  end
+end
