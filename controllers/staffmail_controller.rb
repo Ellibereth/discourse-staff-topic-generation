25f92c5c07fa90433d396256bd9e8a19b9fbd6dd
jose-hms <jose@hms.se>
2018-03-22 15:41:25 -0400

    Function to add user to public topics with no email sent

--- controllers/staffmail_controller.rb
+++ controllers/staffmail_controller.rb
@@ -22,10 +22,30 @@ class ::StaffmailController < ::ApplicationController
     if user.nil?
       user = User.create!(
         email: email_address,
-        username: User.suggest_name(email_address),
+        username: ::StaffmailController.generate_random_user(),
         name: User.suggest_name(email_address),
         staged: true)
     end
     render json: {user: user}
   end
+
+  def add_user_to_topic_no_email
+    topic_id = params[:topic_id]
+    user_id = params[:user_id]
+    username = params[:user_username]
+    private_message = params[:private_message]
+    if !private_message?
+      topic = Topic.find_by(id: topic_id)
+      topic.topic_allowed_users.create(user_id: user_id)
+      TopicUser.auto_notification_for_staging(user_id, topic_id, TopicUser.notification_reasons[:auto_watch])
+      topic.add_small_action(Discourse.system_user, "invited_user", username)
+    end
+    render json: {success: true}
+  end
+
+  #Generate a random username
+def self.generate_random_user
+  name = "anon" + 10.times.map{rand(10)}.join.to_s
+  name
+end
 end
